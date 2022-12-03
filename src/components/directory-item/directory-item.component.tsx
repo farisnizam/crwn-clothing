ee4d6b140e5cf25e9beb31a28778faf3a86cfbe0
farisnizam <farismtq@gmail.com>
2022-12-03 20:18:15 +0800

    convert remaining jsx file to tsx

--- src/components/directory-item/directory-item.component.tsx
+++ src/components/directory-item/directory-item.component.tsx
@@ -1,15 +1,24 @@
-import {
-  DirectoryItemContainer,
-  BackgroundImage,
-  Body,
-} from "./directory-item.styles";
+import { FC } from "react";
 import { useNavigate } from "react-router-dom";
 
-const DirectoryItem = ({ category }) => {
+import { DirectoryCategory } from "../directory/directory.component";
+
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from "./directory-item.styles";
+
+type DirectoryItemProps = {
+  category: DirectoryCategory;
+};
+
+const DirectoryItem: FC<DirectoryItemProps> = ({ category }) => {
   const { imageUrl, title, route } = category;
   const navigate = useNavigate();
 
   const onNavigateHandler = () => navigate(route);
+
   return (
     <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
